44a6c9d28971b2459f2996ef0972cdee73969f27
Mayur Bhardwaj <91755325+Mayur-Bhardwaj@users.noreply.github.com>
2026-05-08 08:40:21 +0530

    Add PageLayout component to App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,29 @@
 import React from "react";
 import './../styles/App.css';
 
+ const PageLayout({ header, footer, children }) => {
+    return (
+      <div>
+    {header}
+    {footer}
+    {children}
+      </div>
+    );
+  };  
+    </div>
+  )
 const App = () => {
   return (
     <div>
         {/* Do not remove the main div */}
-    </div>
-  )
+     <PageLayout
+        header={<h1>Header</h1>}
+        footer={<h3>Footer</h3>}
+      >
+        <p>Main Content</p>
+      </PageLayout>
+          </div>
+  );
 }
 
 export default App
